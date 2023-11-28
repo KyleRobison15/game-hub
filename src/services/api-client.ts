4bee8106dc8adebd44c1e4667b3f5618d67ebbcb
KyleRobison15 <kylerobison15@gmail.com>
2023-11-28 10:19:21 -0700

    Create custom hook to fetch games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,16 @@
 import axios from 'axios';
 
+/**
+ * IMPORTANT: I understand this is bad practice to store an API key in the source code that is published to a git repo.
+ * 
+ * This API key is totally free, and I am not concerned about its visiability for this particular project.
+ * 
+ * Storing this API key securly would require building a custom backend to act as a proxy between the client app and external API,
+ * which would be overkill for this educational project..
+ * 
+ *  
+*/ 
+
 // Here we are configuring our axios instances for making HTTP requests
 // In addition to the standard baseUrl and headers proerties, we must include the params property with an API key
     // This key will be sent along with ALL the HTTP requests we make as a query string
