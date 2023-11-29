310dc2e6a86eccbb9215f1bb0014a6bf4106ef8e
KyleRobison15 <kylerobison15@gmail.com>
2023-11-28 18:03:00 -0700

    Create useGenre hook to fetch genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,12 +1,17 @@
 import { Box } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
+// This is the standard way to define our typescript interface when our component will take other child components as it's Props!!
+// In other words, when we wrap other components with this "GameCardContainer" component,  we expect to recieve it's children as props
+// We can then take the child components and do things with them, in this case wrapping them in a simple styled container
 interface Props {
   children: ReactNode;
 }
 
 const GameCardContainer = ({ children }: Props) => {
   return (
+    // Here we use the generic "Box" component from Chakra UI which is just Chakra's version of a <div>
+    // Doing it this way, gives us access to Chakra's style props
     <Box width={300} borderRadius={10} overflow="hidden">
       {children}
     </Box>
